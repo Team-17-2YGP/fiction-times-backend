a0010d969c8fd2e65c32e521defafe5a04c49f62
Pratheek Senevirathne <pratheeksenevirathne@gmail.com>
2021-12-08 21:23:28 +0530

    Fix response interceptor (#80)

--- src/main/java/com/fictiontimes/fictiontimesbackend/controller/ResponseInterceptor.java
+++ src/main/java/com/fictiontimes/fictiontimesbackend/controller/ResponseInterceptor.java
@@ -1,17 +1,27 @@
 package com.fictiontimes.fictiontimesbackend.controller;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponseWrapper;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResponseInterceptor implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        filterChain.doFilter(servletRequest, servletResponse);
+        final CopyPrintWriter writer = new CopyPrintWriter(servletResponse.getWriter());
+        servletResponse.setContentType("application/json");
+        filterChain.doFilter(servletRequest, new HttpServletResponseWrapper((HttpServletResponse) servletResponse) {
+            @Override
+            public PrintWriter getWriter() {
+                return writer;
+            }
+        });
 
-        String contentType = servletResponse.getContentType();
-        if (contentType == null || contentType.equals("")) {
-            servletResponse.setContentType("application/json");
+        String responseBody = writer.getCopy();
+        if (responseBody == null || responseBody.equals("")) {
+            servletResponse.getWriter().write("{\"status\": \"Successful\"}");
         }
     }
 }
