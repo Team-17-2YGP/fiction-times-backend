841c02043d15253b5b85b354c527c05dba52dadf
Pratheek Senevirathne <pratheeksenevirathne@gmail.com>
2021-12-08 23:23:22 +0530

    Implement get story details endpoints for readers

---
+++ src/main/java/com/fictiontimes/fictiontimesbackend/controller/reader/GetEpisodeListServlet.java
@@ -0,0 +1,28 @@
+package com.fictiontimes.fictiontimesbackend.controller.reader;
+
+import com.fictiontimes.fictiontimesbackend.model.Episode;
+import com.fictiontimes.fictiontimesbackend.repository.GenreRepository;
+import com.fictiontimes.fictiontimesbackend.repository.StoryRepository;
+import com.fictiontimes.fictiontimesbackend.service.StoryService;
+import com.fictiontimes.fictiontimesbackend.utils.CommonUtils;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/reader/story/episodes")
+public class GetEpisodeListServlet extends HttpServlet {
+
+    StoryService storyService = new StoryService(new StoryRepository(), new GenreRepository());
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        int storyId = Integer.parseInt(request.getParameter("id"));
+        List<Episode> episodeList = storyService.getEpisodeListByStoryId(storyId);
+        response.getWriter().write(CommonUtils.getGson().toJson(episodeList));
+    }
+}
