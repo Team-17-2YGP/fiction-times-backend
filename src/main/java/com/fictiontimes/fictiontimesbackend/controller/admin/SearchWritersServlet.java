85db462b26e34d26404ba2092f3b863ba6b7b6ed
Pramudi218 <84280960+Pramudi218@users.noreply.github.com>
2021-12-08 01:30:36 -0800

    Implement unblock user endpoint and search writers endpoint (#75)

---
+++ src/main/java/com/fictiontimes/fictiontimesbackend/controller/admin/SearchWritersServlet.java
@@ -0,0 +1,38 @@
+package com.fictiontimes.fictiontimesbackend.controller.admin;
+
+import com.fictiontimes.fictiontimesbackend.exception.DatabaseOperationException;
+import com.fictiontimes.fictiontimesbackend.model.Writer;
+import com.fictiontimes.fictiontimesbackend.repository.AdminRepository;
+import com.fictiontimes.fictiontimesbackend.service.AdminService;
+import com.fictiontimes.fictiontimesbackend.utils.CommonUtils;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/admin/writers/search")
+public class SearchWritersServlet extends HttpServlet {
+    AdminService adminService = new AdminService(new AdminRepository());
+    private Object String;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, DatabaseOperationException {
+        String parameterValues = request.getParameter("search");
+
+        if (parameterValues != null) {
+            try {
+                int userId = Integer.parseInt(parameterValues);
+                List<Writer> writerList = adminService.searchWritersById(userId);
+                response.getWriter().write(CommonUtils.getGson().toJson(writerList));
+            } catch (NumberFormatException nfe) {
+                List<Writer> writerList = adminService.searchWritersByName(parameterValues);
+                response.getWriter().write(CommonUtils.getGson().toJson(writerList));
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+}
--- src/main/java/com/fictiontimes/fictiontimesbackend/controller/admin/UnblockUserServlet.java
+++ src/main/java/com/fictiontimes/fictiontimesbackend/controller/admin/UnblockUserServlet.java
@@ -1,25 +1,22 @@
 package com.fictiontimes.fictiontimesbackend.controller.admin;
 
 import com.fictiontimes.fictiontimesbackend.exception.DatabaseOperationException;
-import com.fictiontimes.fictiontimesbackend.model.User;
 import com.fictiontimes.fictiontimesbackend.repository.AdminRepository;
 import com.fictiontimes.fictiontimesbackend.service.AdminService;
-import com.fictiontimes.fictiontimesbackend.utils.CommonUtils;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
-@WebServlet("/admin/user/block")
-public class BlockUserServlet extends HttpServlet {
+@WebServlet("/admin/user/unblock")
+public class UnblockUserServlet extends HttpServlet {
     AdminService adminService = new AdminService(new AdminRepository());
 
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException, DatabaseOperationException {
         int userId = Integer.parseInt(request.getParameter("id"));
-        adminService.blockUserByUserId(userId);
+        adminService.unblockUserByUserId(userId);
     }
 }
